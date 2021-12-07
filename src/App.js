e22962f70f82fdf7efc27ab9106fbb22d16d1792
Roman Kolivashko <rkolivashko@gmail.com>
2021-12-06 16:22:28 -0800

    Added shuffling method, imported cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import { useState } from 'react'
+
 import './App.css';
 
+const cardImages = [
+  { "src": "/img/barbeque.png"},
+  { "src": "/img/cupcake.png"},
+  { "src": "/img/hamburger.png"},
+  { "src": "/img/hot-dog.png"},
+  { "src": "/img/pizza-slice.png"},
+  { "src": "/img/sandwich.png"},
+]
+
 function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+  //shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+    .sort(() => Math.random() - 0.5)
+    .map((card) => ({ ...card, id: Math.random() }))
+
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+
+  console.log(cards, turns);
+
   return (
     <div className="App">
       <h2>Memory Game</h2>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
